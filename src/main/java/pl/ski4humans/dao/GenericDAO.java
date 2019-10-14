43d5ad06aacb2bf1ece4f742ebb2b687733c881e
Dawid Bernacki <daw.bernacki@gmail.com>
2019-10-15 00:29:36 +0200

    Main changes: add genericDAO and jpaDAO - add GenericDAO.java - add JpaDAO.java

---
+++ src/main/java/pl/ski4humans/dao/GenericDAO.java
@@ -0,0 +1,18 @@
+package pl.ski4humans.dao;
+
+import java.util.List;
+
+public interface GenericDAO<T> {
+
+    T create(T t);
+
+    T update(T t);
+
+    T get(Object id);
+
+    void delete(Object id);
+
+    List<T> listAll();
+
+    long count();
+}
