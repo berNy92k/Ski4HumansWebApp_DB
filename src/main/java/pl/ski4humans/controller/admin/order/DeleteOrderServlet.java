de1768be4cbe78cdf46a09e49f3c9c7576ef0103
Dawid Bernacki <daw.bernacki@gmail.com>
2020-01-26 00:54:10 +0100

    Main changes: add java servlets for admin\order - add DeleteOrderServlet.java - add ListOrderServlet.java - add ViewOrderServlet.java

---
+++ src/main/java/pl/ski4humans/controller/admin/order/DeleteOrderServlet.java
@@ -0,0 +1,21 @@
+package pl.ski4humans.controller.admin.order;
+
+import pl.ski4humans.service.admin.OrderServices;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/deleteOrder")
+public class DeleteOrderServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        OrderServices orderServices = new OrderServices(req, resp);
+        orderServices.deleteOrder();
+    }
+}
