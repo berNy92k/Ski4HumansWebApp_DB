becf7f860055b3d973910fb9a4dba84a5ab4d587
daw.bernacki <daw.bernacki@gmail.com>
2023-01-19 20:40:58 +0100

    Use lombok in enums classes.

--- src/main/java/pl/ski4humans/enums/CategoryEnum.java
+++ src/main/java/pl/ski4humans/enums/CategoryEnum.java
@@ -1,22 +1,19 @@
 package pl.ski4humans.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum CategoryEnum {
-    SKIS(1),
-    SKISHOES(2),
-    SKIPOLES(3),
-    SKIHELMETS(4),
-    SKIGOGGLES(5),
-    SKIGLOVES(6),
-    THERMOACTIVECLOTHING(7),
-    ZERO(0);
+  SKIS(1),
+  SKISHOES(2),
+  SKIPOLES(3),
+  SKIHELMETS(4),
+  SKIGOGGLES(5),
+  SKIGLOVES(6),
+  THERMOACTIVECLOTHING(7),
+  ZERO(0);
 
-    private int category;
-
-    CategoryEnum(int category) {
-        this.category = category;
-    }
-
-    public int getCategory() {
-        return category;
-    }
+  private final int category;
 }
--- src/main/java/pl/ski4humans/enums/CategoryPLEnum.java
+++ src/main/java/pl/ski4humans/enums/CategoryPLEnum.java
@@ -1,22 +1,19 @@
 package pl.ski4humans.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum CategoryPLEnum {
-    SKIS("NARTY"),
-    SKISHOES("BUTY NARCISRSKIE"),
-    SKIPOLES("KIJKI NARCISRSKIE"),
-    SKIHELMETS("KASKI NARCISRSKIE"),
-    SKIGOGGLES("GOGLE NARCISRSKIE"),
-    SKIGLOVES("RĘKAWICE NARCISRSKIE"),
-    THERMOACTIVECLOTHING("ODZIEŻ TERMOAKTYWANA"),
-    ZERO("BRAK");
+  SKIS("NARTY"),
+  SKISHOES("BUTY NARCISRSKIE"),
+  SKIPOLES("KIJKI NARCISRSKIE"),
+  SKIHELMETS("KASKI NARCISRSKIE"),
+  SKIGOGGLES("GOGLE NARCISRSKIE"),
+  SKIGLOVES("RĘKAWICE NARCISRSKIE"),
+  THERMOACTIVECLOTHING("ODZIEŻ TERMOAKTYWANA"),
+  ZERO("BRAK");
 
-    private String category;
-
-    CategoryPLEnum(String category) {
-        this.category = category;
-    }
-
-    public String getCategory() {
-        return category;
-    }
+  private final String category;
 }
